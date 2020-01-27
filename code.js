b98505bbfb9d6921323c0537ddc7f44db2eb6c0c
Fatih Başkaya <mansger@icloud.com>
2020-01-27 14:48:33 +0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,9 @@
+setInterval(function(){ 
+	  	document.getElementById("me").src = "https://images.pexels.com/photos/1549326/pexels-photo-1549326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=450&w=400";
+	  	$(this).find("#me").fadeOut(1000);
+	  }, 6000);
+	  setInterval(function(){
+	  	document.getElementById("me").src = "https://images.pexels.com/photos/2042109/pexels-photo-2042109.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=450&w=400";
+	  	$(this).find("#me").fadeOut(1000);
+	  }, 12000);
+/*image changing every 6 seconds, you can change it for optional*/
